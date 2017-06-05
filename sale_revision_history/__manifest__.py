30c68a942d834aacfe542ce5a803758cb6207eb1
Riza Thoufique <riza.thoufique@pptservices.com>
2017-06-05 10:56:36 +0530

    Added Sales revision module

---
+++ sale_revision_history/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+{
+	"name": "Sale Revision History",
+	"version": "10.0",
+	"author": "PPTS [India] Pvt.Ltd.",
+	"website": "http://www.pptssolutions.com",
+	"sequence": 0,
+	"depends": [
+		"sale"
+	],
+	"category": "Sales,Invoicing",
+	"complexity": "easy",
+	"description": """
+Quotation sale revision history
+	""",
+	"data": [
+		'views/sale_order_views.xml',
+	],
+	"demo": [
+	],
+	"test": [
+	],
+	"auto_install": False,
+	"installable": True,
+	"application": False,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
