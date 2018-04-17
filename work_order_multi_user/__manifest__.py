e5bfde2ea854df9e3e85bafbc633f5f96e28d561
madhavan.s <madhavan.s@PPT-WSD-A007>
2018-04-17 17:00:24 +0530

    Work Order Multi User

---
+++ work_order_multi_user/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# This file is part of OpenERP. The COPYRIGHT file at the top level of
+# this module contains the full copyright notices and license terms.
+{
+    'name' : 'Work Order multi user',
+    'version': '11.0',
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'category': 'Manufacture',
+    'website':  'https://www.pptssolutions.com',
+    'summary': 'More users in work order',
+    'license': 'LGPL-3',
+    'support': 'business@pptservices.com',
+    'depends': [
+        'mrp_workorder',
+        'mrp'
+        ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/work_order_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
