edc78e5ce208df97b77ecdb644b4121accfec4e0
francis.xavier <francis.xavier>
2018-08-29 15:29:44 +0530

    Added banners

--- bulk_product_publish/__manifest__.py
+++ bulk_product_publish/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Bulk Products Publish',
-    'version': '1.1',
+    'version': '11.00',
     'sequence' : 0,
     'category': 'Website',
     "author": "PPTS [India] Pvt.Ltd.",
@@ -9,12 +9,13 @@
     'license': 'LGPL-3',
     'support': 'business@pptservices.com',
     'description':"""
-                Publishing/Unpublishing bulk products  
+                Publishing/Unpublishing bulk products on website
                       """,
     'depends': ['base','product','sale','website_sale', 'website_mail','decimal_precision','mail','sale_management'],
     'data': [    
                 'wizard/product_publish_wizard_view.xml',      
     ],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'application': True,
     'auto_install': False,
