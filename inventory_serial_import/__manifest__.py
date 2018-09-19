84ab4f93211f059cee18e4cb9bc0bfe31ae325ac
francis.xavier <francis.xavier>
2018-09-19 10:52:22 +0530

    added banner

--- inventory_serial_import/__manifest__.py
+++ inventory_serial_import/__manifest__.py
@@ -17,6 +17,7 @@
         'views/inventory_serial_import_view.xml',
         
     ],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
     'application':True
