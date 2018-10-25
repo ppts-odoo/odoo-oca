6c8c541beab24669d20e86fd1153dccf1d02119a
francis.xavier <francis.xavier>
2018-10-25 16:44:44 +0530

    Issue fix in Account Roundoff

--- account_roundoff/models/account.py
+++ account_roundoff/models/account.py
@@ -71,11 +71,14 @@ class AccountRoundOff(models.Model):
                     from_currency = (line.currency_id and line.currency_id.with_context(
                         date=line.date)) or line.company_id.currency_id.with_context(date=line.date)
                     residual += from_currency.compute(line.amount_residual, self.currency_id)
-        self.residual_company_signed = abs(residual_company_signed) * sign
-        self.residual_signed = abs(residual) * sign
-        if self.round_active is True and self.type == 'out_invoice':
+
+        if self.round_active is True:
+            self.residual_company_signed = round(abs(residual_company_signed)) * sign
+            self.residual_signed = round(abs(residual)) * sign
             self.residual = round(abs(residual))
         else:
+            self.residual_company_signed = abs(residual_company_signed) * sign
+            self.residual_signed = abs(residual) * sign
             self.residual = abs(residual)
         digits_rounding_precision = self.currency_id.rounding
         
@@ -129,11 +132,11 @@ class AccountRoundOff(models.Model):
                     res_amount_currency -= amount_currency or 0
                     if i + 1 == len(totlines):
                         amount_currency += res_amount_currency
-                    if self.round_active is True and self.type == 'out_invoice':
+                    if self.round_active is True:
                         iml.append({
                             'type': 'dest',
                             'name': name,
-                            'price': t[1]+self.round_off_value,
+                            'price': t[1] + (inv.type in ('in_invoice', 'out_refund') and abs(self.round_off_value) or self.round_off_value),
                             'account_id': inv.account_id.id,
                             'date_maturity': t[0],
                             'amount_currency': diff_currency and amount_currency,
@@ -142,11 +145,11 @@ class AccountRoundOff(models.Model):
                         })
                         ir_values = self.env['ir.values']
                         acc_id = ir_values.get_default('account.config.settings', 'round_off_account')
-                        if self.round_off_value > 0:
+                        if self.round_off_value != 0:
                             iml.append({
                                 'type': 'dest',
                                 'name': "Round off",
-                                'price': -self.round_off_value,
+                                'price': (inv.type in ('in_invoice', 'out_refund') and self.round_off_value or -self.round_off_value),
                                 'account_id': acc_id,
                                 'date_maturity': t[0],
                                 'amount_currency': diff_currency and amount_currency,
@@ -167,11 +170,11 @@ class AccountRoundOff(models.Model):
                         })
 
             else:
-                if self.round_active is True and self.type == 'out_invoice':
+                if self.round_active is True:
                     iml.append({
                         'type': 'dest',
                         'name': name,
-                        'price': total + self.round_off_value,
+                        'price': total + (inv.type in ('in_invoice', 'out_refund') and abs(self.round_off_value) or self.round_off_value),
                         'account_id': inv.account_id.id,
                         'date_maturity': inv.date_due,
                         'amount_currency': diff_currency and total_currency,
@@ -180,11 +183,11 @@ class AccountRoundOff(models.Model):
                     })
                     ir_values = self.env['ir.values']
                     acc_id = ir_values.get_default('account.config.settings', 'round_off_account')
-                    if self.round_off_value > 0:
+                    if self.round_off_value != 0:
                         iml.append({
                             'type': 'dest',
                             'name': "Round off",
-                            'price': -self.round_off_value,
+                            'price': (inv.type in ('in_invoice', 'out_refund') and self.round_off_value or -self.round_off_value),
                             'account_id': acc_id,
                             'date_maturity': inv.date_due,
                             'amount_currency': diff_currency and total_currency,
