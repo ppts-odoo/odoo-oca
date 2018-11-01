8d80bd08a7eb59e7d52ec2630c7bf02781ca2ea2
francis.xavier <francis.xavier>
2018-11-01 16:53:59 +0530

    added clear shopping cart module

---
+++ vacuum_shopping_cart/controllers/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import json
+import logging
+from werkzeug.exceptions import Forbidden, NotFound
+
+from odoo import http, tools, _
+from odoo.http import request
+from odoo.addons.base.ir.ir_qweb.fields import nl2br
+from odoo.addons.website.models.website import slug
+from odoo.addons.website.controllers.main import QueryURL
+from odoo.exceptions import ValidationError
+from odoo.addons.website_form.controllers.main import WebsiteForm
+
+_logger = logging.getLogger(__name__)
+
+PPG = 20  # Products Per Page
+PPR = 4   # Products Per Row
+
+
+class WebsiteSale(http.Controller):
+
+    @http.route(['/shop/clear_cart'], type='json', auth="public", website=True)
+    def clear_cart(self):
+        order = request.website.sale_get_order()
+        if order:
+            for line in order.website_order_line:
+                line.unlink()
