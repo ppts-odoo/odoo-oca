0a68215b8bc20e50acd26f5ff69ab72aceed9960
francis.xavier <francis.xavier>
2019-07-11 17:51:06 +0530

    added new modules

--- work_order_multi_user/__manifest__.py
+++ work_order_multi_user/__manifest__.py
@@ -8,7 +8,7 @@
     'category': 'Manufacturing',
     'website':  'https://www.pptssolutions.com',
     'summary': 'More users in work order',
-    'license': 'OEEL-1',
+    'license': 'OPL-1',
     'support': 'business@pptservices.com',
     'depends': [
         'mrp_workorder',
