b8c773d5dd122cd4f8ad344743a36f8975e43289
Francis Xavier <francis.xavier@pptservices.com>
2020-01-27 12:17:07 +0530

    added pricelist module

---
+++ pricelist_product_price_import/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pricelist Product Price Import',
+    'summary': ' This module allows you to import products to line items based on product category.',
+    'version': '10',
+    'category': 'sale',
+    'description': """
+       Configuration:
+       In Import Product Category model a  button called import is added in product pricelist.
+       When we click import button products under the product category is added to line items.
+    """,
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'website': 'https://www.pptssolutions.com',
+    'depends': ['base', 'product', 'sale'],
+    'data': [
+        
+        'views/import_products_view.xml',
+        
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application':True,
+    'active': False,
+}
+
+
