dffe93d96f20162ecb4a68173994624e8948ee59
Francis Xavier S <francis.xavier@pptservices.com>
2020-07-21 10:23:15 +0530

    canned response module added

---
+++ canned_responses_global/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Canned Response',
+    'website': 'https://www.pptssolutions.com/',
+    'author': 'PPTS India Pvt Ltd',
+    'summary': 'Canned Response',
+    'version': '1.1',
+    'license': 'LGPL-3',
+    'description': """
+    Canned Response
+        """,
+    'depends': ['mail','base'],
+    'data': [
+        'views/canned_response.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': True,
+}
