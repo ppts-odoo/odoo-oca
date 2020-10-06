d4f526ff7a1002420d729dc5c13e0b1a577fd074
Francis Xavier S <francis.xavier@pptservices.com>
2020-10-06 09:30:38 +0530

    sale approval change

--- sale_approval/models/res_users.py
+++ sale_approval/models/res_users.py
@@ -5,8 +5,8 @@ class Users(models.Model):
     _inherit = "res.users"
     
     sale_order_can_approve = fields.Selection([('yes', 'Yes'), ('no', 'No')], 'Can Approve Sale?',default='no')
-    sale_order_amount_limit = fields.Float("(SO) Amount Limit", digits=(16, 0), required=True)
-    sale_order_discount_limit = fields.Float("(SO) Discount Limit", digits=(16, 0), required=True)
+    sale_order_amount_limit = fields.Float("(SO) Amount Limit", digits=(16, 0))
+    sale_order_discount_limit = fields.Float("(SO) Discount Limit", digits=(16, 0))
 
 
 
