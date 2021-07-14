092f50934c3c2412c675e78c80be19919aa37cdf
Francis Xavier S <francis.xavier@pptservices.com>
2021-07-14 10:36:17 +0530

    crm opportunity product update

--- crm_opportunity_product/models/opportunity_product.py
+++ crm_opportunity_product/models/opportunity_product.py
@@ -26,31 +26,35 @@ class CrmLead(models.Model):
     lead_product_ids = fields.One2many('crm.lead.product','lead_id',string='Products For Quotation')
 
     def action_create_quotation(self):
-        sale_obj=self.env['sale.order']
-        sale_line_obj=self.env['sale.order.line']
         order_lines = []
         for line in self.lead_product_ids:
             order_lines.append((0,0,{'product_id': line.product_id.id,
                 'name': line.description,
                 'product_uom_qty':line.qty,
+                'product_uom': line.product_uom.id,
                 'price_unit': line.price_unit,
                 'tax_id':[(6, 0, line.tax_id.ids)]
-
             }))
+
         if self.partner_id:
-            sale_id = sale_obj.create({
-                'partner_id':self.partner_id.id,
-                'team_id': self.team_id.id,
-                'campaign_id': self.campaign_id.id,
-                'medium_id': self.medium_id.id,
-                'source_id': self.source_id.id,
-                'opportunity_id': self.id,
-                'order_line':order_lines,
+            vals = ({
+                'default_partner_id':self.partner_id.id,
+                'default_team_id': self.team_id.id,
+                'default_campaign_id': self.campaign_id.id,
+                'default_medium_id': self.medium_id.id,
+                'default_source_id': self.source_id.id,
+                'default_opportunity_id': self.id,
+                'default_order_line':order_lines,
             })
         else:
-            raise UserError('In order to create sale order, Customer field should not be empty !!!')
-        return True
-    
-    
-    
-    
+            raise UserError('In order to create sale order, Customer field should not be empty!')
+
+        return {
+            'name': "Create Sale Quotation",
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'sale.order',
+            'target': 'current',
+            'context': vals,
+        }
