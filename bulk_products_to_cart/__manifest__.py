40f1a08325919c2a33715b3ab37b8de14681f3d6
Francis <francis.xavier@pptservices.com>
2022-10-15 12:30:09 +0530

    bulk products to cart

--- bulk_products_to_cart/__manifest__.py
+++ bulk_products_to_cart/__manifest__.py
@@ -10,7 +10,7 @@
     'data': ['views/export_button.xml'],
     'demo': [],
     'images': ['static/description/banner.png'],
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
 }
