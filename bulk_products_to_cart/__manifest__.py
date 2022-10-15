881c0a2eb792debc00077fef072916d54dca989d
Francis <francis.xavier@pptservices.com>
2022-10-15 12:18:21 +0530

    bulk products to cart

---
+++ bulk_products_to_cart/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bulk Products To Cart on Website',
+    'version': '14.0',
+    'summary': """ An OCA Module to add bulk products on the shopping cart by exporting and Importing process.""",
+    'description': '',
+    'category': 'Website',
+    'author': 'PPTS [India] Pvt.Ltd.',
+    'depends': ['base', 'website_sale', 'report_xlsx'],
+    'data': ['views/export_button.xml'],
+    'demo': [],
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
