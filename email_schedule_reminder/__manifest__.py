25ef5ab5ce83d8744f5e86eebeb0c44f5b571a94
Francis <francis.xavier@pptservices.com>
2022-11-21 10:28:24 +0530

    added email schedule module

---
+++ email_schedule_reminder/__manifest__.py
@@ -0,0 +1,24 @@
+
+{
+    'name': 'Month End Email Scheduler',
+    'version': '16.0',
+    'author': 'PPTS [India] Pvt.Ltd',
+    'category': 'General',
+    'license': 'LGPL-3',
+    'website': 'https://www.pptssolutions.com',
+    'depends': ['base', 'mail'],
+    'description': """ 
+    This module will generate emails based on the selected templates at every month end.""",
+    'data': [
+        'security/ir.model.access.csv',
+        'data/email_reminder_cron.xml',
+        'views/email_scheduler_view.xml',
+        'views/email_scheduler_menu_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
+
