42d816b338cd0c5b32cf34ff8c5e0f3eb9b85e35
ravishankar.m <ravisankar.m@pptservices.com>
2024-12-18 11:18:32 +0530

    Website product name module deleted in version 11

--- website_product_name/models/product.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api,_
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    web_pro_name = fields.Char(string="Website Product Name", translate=True, index=True)
